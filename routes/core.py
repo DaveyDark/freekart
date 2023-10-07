5d4bb9d1c5163c64b864a5dc1477f45db9c4d35a
DaveyDark <devesh97531@gmail.com>
2023-10-08 00:18:55 +0530

    Work on edit page

--- routes/core.py
+++ routes/core.py
@@ -1,5 +1,5 @@
 from flask import Blueprint , render_template
-from models import db, Product
+from models import db, Product, Seller
 from datetime import datetime
 from flask import Blueprint, redirect , render_template, session, url_for
 
@@ -31,7 +31,18 @@ def logout():
     return redirect('/')
 @core.route("/dashboard/")
 def dashboard():
-    return render_template("dashboard.html")
+    seller = auth_seller()
+    if not seller:
+        return redirect(url_for('core.root'))
+    return render_template("dashboard.html", seller=seller)
 @core.route("/dashboard/add/")
 def add():
     return render_template("add.html")
+
+def auth_seller():
+    if 'user_id' not in session or 'type' not in session or session['type'] != 'seller':
+        return None
+    seller = Seller.query.get(session['user_id'])
+    if not seller:
+        return None
+    return seller
