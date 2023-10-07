3a88215f43514b0bf3bad6e8bb5c98386195b349
Ekuspreet <ekuspreetsingh2@gmail.com>
2023-10-07 15:18:03 +0530

    Some Ui CHanges

--- routes/core.py
+++ routes/core.py
@@ -1,7 +1,7 @@
-from flask import Blueprint 
+from flask import Blueprint , render_template
 
 core = Blueprint('core', __name__)
 
 @core.route("/")
 def hello():
-    return "Hello World!"
+    return render_template("landing.html")
