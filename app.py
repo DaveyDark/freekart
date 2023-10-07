99bb8230e24856c87ce1eddcc57bc7b96c305801
DaveyDark <devesh97531@gmail.com>
2023-10-07 13:05:45 +0530

    Added routes and models files

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, redirect, render_template, session, url_for
+from routes.api import api
+from routes.core import core
+from models import db
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+app = Flask(__name__)
+app.secret_key = "apex_secret"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///utype.db"
+
+db.init_app(app)
+
+app.register_blueprint(api, url_prefix='/api')
+app.register_blueprint(core, url_prefix='/')
