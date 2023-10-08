020e0054019a2d8340c33d718b106fb4924e2809
Amrinderdeep Singh Bhatt <addybhatt22@gmail.com>
2023-10-08 09:35:38 +0530

    Time formatting

--- routes/core.py
+++ routes/core.py
@@ -49,9 +49,12 @@ def dashboard():
     if not seller:
         return redirect(url_for('core.root'))
     orders = Order.query.join(Product).filter(Product.seller_id == seller.id).all()
+    for product in seller.products:
+        product.time = product.expiry.strftime("%d-%m-%Y")
     for order in orders:
         order.product_name = Product.query.get(order.product_id).name
         order.customer_name = Customer.query.get(order.customer_id).name
+        order.time = order.timestamp.strftime("%d-%m-%Y  %H:%M %p")
     return render_template("dashboard.html", seller=seller, orders=orders)
 
 @core.route("/profile/")
@@ -62,6 +65,7 @@ def profile():
     orders = Order.query.filter_by(customer_id=session["user_id"]).all()
     for order in orders:
         order.product_name = Product.query.get(order.product_id).name
+        order.timestamp = order.timestamp.strftime("%d-%m-%Y  %H:%M %p")
     return render_template("profile.html", customer=customer, orders=orders)
 @core.route("/dashboard/add/")
 def add():
